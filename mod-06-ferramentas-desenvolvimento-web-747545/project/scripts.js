6ce5577aa33236fa901412868f352a1bad71af92
Mário Júnior <mariojuniorprofissional@gmail.com>
2025-07-07 20:53:35 -0300

    refactor: Minor adjustments

--- mod-06-ferramentas-desenvolvimento-web-747545/project/scripts.js
+++ mod-06-ferramentas-desenvolvimento-web-747545/project/scripts.js
@@ -1,3 +1,4 @@
+// Seleciona minha imagem do perfil e adiciona o som quando o mouse passa por cima
 const imgPerfil = document.querySelector('.foto-perfil img');
 const som = document.getElementById('somPerfil');
 
